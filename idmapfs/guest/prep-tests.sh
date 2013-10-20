6c088ee05505124e376b4e5cf6240e1f3d52f3ac
Alin Dobre <alinmd@gmail.com>
2013-10-20 22:25:20 +0100

    Nullfs tests
    Signed-off-by: Alin Dobre <alinmd@gmail.com>

--- idmapfs/guest/prep-tests.sh
+++ idmapfs/guest/prep-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
-umount --detach-loop ${FS_MOUNT_PLAIN} ${FS_MOUNT_WRAP} || :
-rmmod idmapfs || :
+#umount --detach-loop ${FS_MOUNT_PLAIN} ${FS_MOUNT_WRAP} || :
+#rmmod idmapfs || :
 mkdir -pv ${FS_MOUNT_PLAIN} ${FS_MOUNT_WRAP}
+modprobe -d ${DATA_ROOT} ext2
 modprobe -d ${DATA_ROOT} idmapfs
-mount ${DATA_ROOT}/ext2fs.loop.orig ${FS_MOUNT_PLAIN} -o loop
+mount ${DATA_ROOT}/ext2fs.loop.orig ${FS_MOUNT_PLAIN} -o loop -t ext2
 mount -t idmapfs ${FS_MOUNT_PLAIN} ${FS_MOUNT_WRAP}
--- idmapfs/guest/trace.sh
+++ idmapfs/guest/trace.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -xe
 
-if [ "$1" != "-n" ]; then
-	if grep -qE ' /mnt/.*fs ' /etc/mtab; then
-		umount --detach-loop /mnt/{wrap,}fs
-	fi
-	rmmod wrapfs || :
-	insmod /mnt/data/wrapfs.ko
-	cp -va /mnt/data/btrfs.loop.orig /tmp/btrfs.loop
-	mount /tmp/btrfs.loop /mnt/fs -o loop
-	mount -t wrapfs /mnt/fs /mnt/wrapfs
-else
-	shift
-fi
-
 cd /sys/kernel/debug/tracing
 echo nop > current_tracer
 #echo function > current_tracer
@@ -23,9 +10,9 @@ echo nofuncgraph-overhead > trace_options
 #echo 100000 > buffer_size_kb
 echo $$ > set_ftrace_pid
 #echo > set_ftrace_pid
-echo > 			set_ftrace_filter
-echo ':mod:wrapfs' >>	set_ftrace_filter
-echo ':mod:btrfs' >>	set_ftrace_filter
+echo > set_ftrace_filter
+echo ':mod:idmapfs' >>	set_ftrace_filter
+echo ':mod:ext2' >>	set_ftrace_filter
 #echo 'd_instantiate' >>	set_ftrace_filter
 echo 1 > tracing_on
 ${@} || :
